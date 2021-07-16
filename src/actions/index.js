aa1671caa76375aa01a116ceb6cdf4f1dacd0f97
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-07-16 11:43:01 -0700

    finished setting up actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,47 @@
 import axios from 'axios';
 
+export const FETCH_START = 'FETCH_START';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAIL = 'FETCH_FAIL';
+export const ADD_SMURF = 'ADD_SMURF';
+export const SET_ERROR = 'SET_ERROR';
+
+export const fetchSmurfs = () => {
+  return (dispatch) => {
+    dispatch(fetch());
+    const url = 'http://localhost:3333/smurfs'
+    axios
+      .get(url)
+      .then(function(response) {
+        dispatch(fetchSuccess(response.data))
+      })
+      .catch(function (error) {
+        fetchFail(error)
+        console.log("the error", error);
+      });
+  }
+}
+
+export const fetchStart = () => {
+  return ({ type: FETCH_START });
+}
+
+export const fetchSuccess = (smurfs) => {
+  return ({ type: FETCH_SUCCESS, payload:smurfs });
+}
+
+export const fetchFail = (error) => {
+  return ({ type: FETCH_FAIL, payload:error })
+}
+
+export const addSmurf = (payload) => {
+  return ({ type: ADD_SMURF, payload })
+}
+
+export const setError = (error) => {
+  return ({ type: SET_ERROR, payload:error })
+}
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
